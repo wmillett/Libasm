60a1df11d161b083153f43469253d19fc54b2fe3
William Millette <wmillett@c1r6p1.42quebec.com>
2024-08-30 16:31:46 -0400

    commented problematic line in str_len

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -4,7 +4,7 @@ global ft_strlen
 section .text
 ft_strlen:
     xor eax, eax         ; Initialize length counter to 0
-    mov edi, rdi         ; Copy pointer to string to rdi
+    ; mov edi, rdi         ; Safer to remove this line and stay in rdi to avoid losing data from moving to 32 bit value
 strlen_loop:
     cmp byte [rdi], 0    ; Compare current byte to 0
     je strlen_done       ; If zero, jump to done
