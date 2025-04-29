84afb64c2b30ecf12b3578183c4b336f438ef58d
wmillett <your_email@example.com>
2025-04-29 15:00:23 +0200

    added colour to test messages

--- test/test_main.c
+++ test/test_main.c
@@ -10,11 +10,18 @@ void test_ft_strcpy();
 
 int main() {
     // Run all tests
+    printf("\033[31m");
     //test_ft_strdup(); //TODO: fix strdup so it does not segfault
+    printf("\033[32m");
     test_ft_strlen();
+    printf("\033[33m");
     test_ft_write();
+    printf("\033[34m");
     test_ft_read();
+    printf("\033[35m");
     test_ft_strcmp();
+    printf("\033[36m");
     test_ft_strcpy();
+    printf("\033[0m");
     return 0;
 }
